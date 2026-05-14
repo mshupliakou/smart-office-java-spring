7b240ad23ca3aec5f0c301e39a7bc8593fa5b26a
mshupliakou <mikhail.shupliakou@gmail.com>
2026-05-14 12:59:46 +0200

    Initial commit

---
+++ src/main/java/com/project_agh/smartoffice/SmartOfficeApplication.java
@@ -0,0 +1,13 @@
+package com.project_agh.smartoffice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartOfficeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartOfficeApplication.class, args);
+	}
+
+}
