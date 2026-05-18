de9f2112d634d46e799f2ec406b8a9aeeab03d43
mshupliakou <mikhail.shupliakou@gmail.com>
2026-05-18 18:13:09 +0200

    fix

--- src/main/java/com/project_agh/smartoffice/model/Worker.java
+++ src/main/java/com/project_agh/smartoffice/model/Worker.java
@@ -2,16 +2,16 @@ package com.project_agh.smartoffice.model;
 
 import lombok.Data;
 
-import java.util.UUID;
-
 import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.Transient;
+import org.springframework.data.domain.Persistable;
 import org.springframework.data.relational.core.mapping.Column;
 import org.springframework.data.relational.core.mapping.Table;
 
 
 @Table("worker")
 @Data
-public class Worker {
+public class Worker implements Persistable<String> {
     @Id
     @Column("id")
     private String id;
@@ -21,4 +21,17 @@ public class Worker {
     private String lastname;
     @Column("jobtitle")
     private String jobTitle;
+
+    @Transient
+    private boolean isNew = true;
+
+    @Override
+    public String getId() {
+        return this.id;
+    }
+
+    @Override
+    public boolean isNew() {
+        return this.isNew;
+    }
 }
